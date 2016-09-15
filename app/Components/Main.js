4bea221c294928c344bca9d05e761a5df2367b41
Daniel Lopez <Daniel Lopez>
2016-09-14 23:31:03 -0400

    upload2

---
+++ app/Components/Main.js
@@ -0,0 +1,18 @@
+var react = require('react');
+
+var Saved = require('Saved.js');
+var Search = require('Search.js');
+
+var Main = React.createClass({
+
+getInitialState: function(){
+
+	return{
+
+	}
+
+},
+
+
+
+})
---
+++ app/Components/Saved.js
@@ -0,0 +1,30 @@
+
+
+var React = require('react');
+
+var Search = require('Search');
+
+var Saved = React.createClass({
+	getInitialState: function(){
+		return{
+
+		}
+	},
+
+	render: function(){
+
+		return (
+
+			<div>
+
+			</div>
+
+		)
+
+	}
+
+
+
+});
+
+module.exports = Saved;
